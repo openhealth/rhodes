5d14bbad3373bacff72df89dc7fbf4a75813ee20
Chris Howard <choward@openhealth.com.au>
2009-06-24 15:48:40 +1000

    Modified find method for faster synching.

--- rhodes/rhodes-framework/lib/rhom/rhom_object_factory.rb
+++ rhodes/rhodes-framework/lib/rhom/rhom_object_factory.rb
@@ -89,62 +89,78 @@ module Rhom
                 def find(*args)
                   puts "Inside find: args - #{args.inspect}"
                   ret_list = []
-                  conditions = {}
-                  where_cond = nil
-                  # first find all query objects
-                  if args.first == :all
-                    where_cond = {"source_id"=>get_source_id}
-                  elsif args.first.is_a?(String)
-                    where_cond = {"object"=>strip_braces(args.first.to_s)}
-                  end
-
-                  # do we have conditions?
-                  # if so, add them to the query
+                  # do we have conditions? let's make a conditions hash
                   condition_hash = {}
                   select_arr = nil
                   if args[1]
                     condition_hash = args[1][:conditions] if args[1] and args[1][:conditions] and args[1][:conditions].is_a?(Hash)
                     select_arr = args[1][:select] if args[1][:select]
                   end
-                  
-				          start = Time.new
-                  # return horizontal resultset from database
-                  # for example, an object that has attributes name,industry:
-                  # |               object                 |       name         |  industry   |
-                  # ---------------------------------------------------------------------------
-                  # | 3560c0a0-ef58-2f40-68a5-48f39f63741b |A.G. Parr PLC 37862 |Entertainment|
-                  attribs = get_attribs
-                  if attribs and attribs.length > 0
-                    sql = ""
-                    sql << "SELECT * FROM (\n" if condition_hash.length > 0
-                    sql << "SELECT object,\n"
-                    attribs.reject! {|attrib| select_arr.index(attrib).nil?} if select_arr
-                    start = Time.new
-                    attribs.each do |attrib|
-                      unless attrib.nil? or attrib.length == 0 or method_name_reserved?(attrib)
-                        sql << "MAX(CASE WHEN attrib = '#{attrib}' AND update_type IN (#{::Rhom::UPDATE_TYPES.join(',')}) THEN value ELSE NULL END) AS \"#{attrib}\",\n"
-                      end
-                    end 
-                    sql.chomp!
-                    sql.chop!
-                    sql << " FROM object_values ov where update_type not in ('delete','update')\n"
-                    sql << "AND " + ::Rhom::RhomDbAdapter.where_str(where_cond) + "\n" if where_cond and where_cond.length > 0
-                    sql << "group by object\n"
-                    sql << "order by \"#{args[1][:order]}\"" if args[1] and args[1][:order]
-                    sql << ") WHERE " + ::Rhom::RhomDbAdapter.where_str(condition_hash) if condition_hash.length > 0
-                  
-                    list = ::Rhom::RhomDbAdapter.execute_sql(sql)
-                    puts "Database query took #{Time.new - start} sec, #{list.length} rows"
-                    start = Time.new
-                    list.each do |rowhash|
-                      # always return object field with surrounding '{}'
-                      rowhash['object'] = "{#{rowhash['object']}}"
-                      new_obj = self.new
-                      new_obj.vars.merge!(rowhash)
-                      ret_list << new_obj
-                    end
-                  end
-                  puts "Processing rhom objects took #{Time.new - start} sec, #{ret_list.length} objects"
+                  source_id = get_source_id
+                  sql = ""
+				  # find by :conditions
+				  if condition_hash.length > 0
+				    #compose SQL for selection of object IDs
+				    table = " object_values " # First select will be from object_values table
+				    i = 0
+					condition_hash.each do |k , v| # add each condition as nested select
+					  sql << "SELECT object FROM"
+  					  sql << " (" if i > 0  # outer selects are from previous results
+					  sql << table
+					  sql << ") " if i > 0
+					  sql << " WHERE "
+					  sql << "attrib ='" << k.to_s << "' AND value='" << v.to_s << "' AND source_id=" << source_id.to_s
+					  table = sql.dup
+					  i += 1
+					end
+                  elsif args.first == :all
+				  # find :all
+					sql << "SELECT distinct(object) FROM object_values WHERE source_id=" << source_id.to_s
+                  elsif args.first == :first
+				  # find :first
+					sql << "SELECT object FROM object_values WHERE source_id=" << source_id.to_s << " LIMIT 1"
+                  elsif args.first.is_a?(String)
+				  # find by objcet ID
+					sql << "SELECT object FROM object_values WHERE source_id=" << source_id.to_s << " AND object='" << strip_braces(args.first.to_s) << "'"
+				  end
+				  puts "Object Lookup SQL is: #{sql}"
+				  start = Time.new
+				  objects = ::Rhom::RhomDbAdapter.execute_sql(sql)
+				  puts "Object Lookup query took #{Time.new - start} sec, #{objects.length} objects"
+				  puts "Objects: #{objects.inspect.to_s}"
+				  # Now we have the object IDs, lets find the attributes for each object
+				  objects.uniq!
+				  start = Time.new
+				  objects.each do |object|
+				    object_id = object.values[0] 
+					sql = "SELECT * from object_values WHERE object='#{object_id}'" 
+					object = ::Rhom::RhomDbAdapter.execute_sql(sql)
+				    puts "Looked up object (#{object_id}): #{object.inspect.to_s}"
+					# create the new object
+					new_obj = self.new
+					# Add the object attribute first with {}
+ 				    new_obj.vars.merge!( 'object' => "{#{object_id}}")
+					# and add each attribute
+					object.uniq!
+#					msg = "Added attributes for object (#{object_id}): "
+					object.each do |rowhash|
+					  new_obj.vars.merge!(rowhash['attrib'] => rowhash['value'])
+#					  msg << rowhash['attrib']  + ":" + rowhash['value'] + ";"
+					end
+					puts "Composed object (#{object_id}): " + object.inspect.to_s
+#					puts msg.to_s
+					# put it on the list to return
+					ret_list << new_obj
+				  end
+                  puts "Composing rhom objects took #{Time.new - start} sec, #{ret_list.length} objects"
+				  start = Time.new
+				  order_value = args[1][:order] if args[1] and args[1][:order]
+ 			      if order_value
+				    order_sym = order_value.to_sym
+#					ret_list.sort! {|x,y| x.send(order_sym) && y.send(order_sym) ? x.send(order_sym) <=> y.send (order_sym) : 0}
+					ret_list.sort!
+                    puts "Sorting objects by #{order_value} took #{Time.new - start} sec"
+				  end
                   args.first == :first || args.first.is_a?(String) ? ret_list[0] : ret_list
                 end
               
@@ -222,29 +238,19 @@ module Rhom
                 # iterate over each instance variable and insert create row to table
 				        obj = self.inst_strip_braces(self.object)
 				        update_type=self.get_update_type_by_source('create')
-                begin
-                    ::Rhom::RhomDbAdapter.start_transaction
-                
-                    self.vars.each do |key,value|
-                      val = self.inst_strip_braces(value)
-                      # add rows excluding object, source_id and update_type
-                      unless self.method_name_reserved?(key)
-                        fields = {"source_id"=>self.get_inst_source_id,
-                                  "object"=>obj,
-                                  "attrib"=>key,
-                                  "value"=>val,
-                                  "update_type"=>update_type}
-                        fields = key == "image_uri" ? fields.merge!({"attrib_type" => "blob.file"}) : fields
-                        result = ::Rhom::RhomDbAdapter.insert_into_table('object_values', fields)                                     
-                      end
-                    end
-                    
-                    ::Rhom::RhomDbAdapter.commit
-
-                rescue Exception => e
-                    ::Rhom::RhomDbAdapter.rollback    
-                end    
-                
+                self.vars.each do |key,value|
+                  val = self.inst_strip_braces(value)
+                  # add rows excluding object, source_id and update_type
+                  unless self.method_name_reserved?(key)
+                    fields = {"source_id"=>self.get_inst_source_id,
+                              "object"=>obj,
+                              "attrib"=>key,
+                              "value"=>val,
+                              "update_type"=>update_type}
+                    fields = key == "image_uri" ? fields.merge!({"attrib_type" => "blob.file"}) : fields
+                    result = ::Rhom::RhomDbAdapter.insert_into_table('object_values', fields)                                     
+                  end
+                end
                 result
               end
           
